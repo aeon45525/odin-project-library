46ea1b3ba1125da9b252935aeca3c6ee373b523e
aeon45525 <aaronjosephcatapang12@gmail.com>
2026-04-27 20:58:18 +0800

    script to display book info on html need refraction to reduce redundancy

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function addBookToLibrary(title, author, pages, year) {
   myLibrary.push(newBook);
 }
 
+// sample books
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "310 pages", "1937");
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "281 pages", "1960");
 addBookToLibrary("1984", "George Orwell", "328 pages", "1949");
@@ -36,3 +37,24 @@ addBookToLibrary("Moby Dick", "Herman Melville", "635 pages", "1851");
 addBookToLibrary("The Alchemist", "Paulo Coelho", "208 pages", "1988");
 
 console.log(myLibrary);
+
+// DOM Manipulators
+const container = document.querySelector(".container");
+
+for (let i = 1; i <= myLibrary.length; i++) {
+  const bookDiv = document.createElement("div");
+  const titleDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const pagesDiv = document.createElement("div");
+  const yearDiv = document.createElement("div");
+  const book = myLibrary[i - 1];
+
+  titleDiv.textContent = book.title;
+  authorDiv.textContent = book.author;
+  pagesDiv.textContent = book.pages;
+  yearDiv.textContent = book.year;
+
+  bookDiv.append(titleDiv, authorDiv, pagesDiv, yearDiv);
+  bookDiv.classList.add("bookDiv");
+  container.append(bookDiv);
+}
